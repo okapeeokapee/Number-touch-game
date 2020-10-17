085e7d9c88c69d7d425d6ee47685a68c3248b96a
okapeeokapee <okapee420@gmail.com>
2020-10-17 22:56:10 +0900

    class add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 'use strict';{
+  class Board{
+    constructor(){
+      this.panels = [];
+      for(let i = 0; i<4; i++){
+        this.panels.push(new this.panels());
+      }
+    }
+  }
 
+  const board = new Board();
 }
